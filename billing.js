1a5d6154e99ab5b85d36d2b957bcc409055f9b8f
ManoliShah03 <manoli.shah@sigmainfo.net>
2023-02-23 12:12:46 +0530

    my first update

--- billing.js
+++ billing.js
@@ -1,270 +1,115 @@
-var ItemValue, ItemQuantity;
-var priceToPay;
+function copyAddress() {
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    var street = document.getElementById("street");
+    var postal = document.getElementById("postal");
+    var city = document.getElementById("city");
+    var country = document.getElementById("country");
+
+    var fbname = document.getElementById("fbname");
+
+    var lbname = document.getElementById("lbname");
+   
+    var bstreet = document.getElementById("bstreet");
+    var bpostal = document.getElementById("bpostal");
+    var bcity = document.getElementById("bcity");
+    var bcountry = document.getElementById("bcountry");
+    var check = document.getElementById("check");
 
 
-document.getElementById("validationBtn").addEventListener("click", function(event){
-    event.preventDefault()
-  });
+    if (check.checked == true) {
+        fbname.value = fname.value;
+        lbname.value = lname.value;
+        bstreet.value = street.value;
+        bpostal.value = postal.value;
+        bcity.value = city.value;
+        bcountry.value = country.value;
 
+    }
+    else {
+        fbname.value = "";
+        lbname.value = "";
+        bstreet.value = "";
+        bpostal.value = "";
+        bcity.value = "";
+        bcountry.value = "";
+    }
 
-//form validation
+}
+
 function validateForm() {
     
-    var flag = false;
-console.log("manoli");
-console.log("test");
-    var ship_first_name, ship_last_name, ship_street_add,  ship_city, ship_postal, ship_state;
-    var bill_first_name, bill_last_name, bill_street_add, bill_city, bill_postal, bill_state;
+    var fname = document.forms.address.fname.value;
+    var lname = document.forms.address.lname.value;
+    var street = document.forms.address.address.value;
+    var postal = document.forms.address.postal.value;
+    var city = document.forms.address.city.value;
+    var country = document.forms.address.country.value;
 
-    ship_first_name = document.forms["myForm"]["ShippingAd"].value;
-    ship_last_name = document.forms["myForm"]["ShippingAd1"].value;
-    ship_street_add = document.forms["myForm"]["ShippingAd2"].value;
-    ship_city = document.forms["myForm"]["ShippingAd6"].value;
-    ship_postal = document.forms["myForm"]["ShippingAd9"].value;
-    ship_state = document.forms["myForm"]["ShippingAd12"].value;
-
-    bill_first_name = document.forms["myForm"]["BillingAd"].value;
-    bill_last_name = document.forms["myForm"]["BillingAd1"].value;
-    bill_street_add = document.forms["myForm"]["BillingAd2"].value;
-    bill_city = document.forms["myForm"]["BillingAd6"].value;
-    bill_postal = document.forms["myForm"]["BillingAd9"].value;
-    bill_state = document.forms["myForm"]["BillingAd12"].value;
-
-// first name validation
     var regName = /^[A-Za-z]+$/;
+    var regnum = /^[0-9]*$/;
+    var letters = /^[A-Za-z]+$/;
 
-    if(ship_first_name == "" || ship_first_name==null){
-        alert('First Name cannot be empty');
+    if (!regName.test(fname)) {
+        window.alert("First Name incorrect");
+        return false;
+    }
+    if (!regName.test(lname)) {
+        window.alert("Last Name incorrect");
         return false;
     }
 
-    if(!(regName.test(ship_first_name)))
-    {
-        alert('Invalid First Name given');
+    if (postal.length < 5 || !regnum.test(postal)) {
+        window.alert("Postal Code Incorrect")
+        return false;
+    }
+    if (!letters.test(country)) {
+        window.alert("Country Incorrect")
+        return false;
+
+    }
+
+    if (!letters.test(city)) {
+        window.alert("City Incorrect")
+        return false;
+
+    }
+
+    var fbname = document.forms.address.fbname.value;
+    var lbname = document.forms.address.lbname.value;
+    var bstreet = document.forms.address.baddress.value;
+    var bpostal = document.forms.address.bpostal.value;
+    var bcity = document.forms.address.bcity.value;
+    var bcountry = document.forms.address.bcountry.value;
+
+    if (!regName.test(fbname)) {
+        window.alert("First Name incorrect");
+        return false;
+    }
+    if (!regName.test(lbname)) {
+        window.alert("Last Name incorrect");
         return false;
     }
 
-
-    if ((ship_first_name.length <= 2) || (ship_first_name.length > 20)) {
-        alert("To Less Characters in First Name for Shipping Adress");
+        if (bpostal.length < 5 || !regnum.test(bpostal)) {   
+        window.alert("Postal Code Incorrect")
+        return false;
+    }
+    if (!letters.test(bcountry)) {
+        window.alert("Country Incorrect")
         return false;
     }
 
-// last name validation
-    var regName = /^[A-Za-z]+$/;
-    if(ship_last_name == "" || ship_last_name==null){
-        alert('Last Name cannot be empty');
+    if (!letters.test(bcity)) {
+        window.alert("City Incorrect")
         return false;
+
     }
-    if(!(regName.test(ship_last_name)))
-    {
-        alert('Invalid Last Name given');
-        return false;
-    }
+  
+    window.alert("Order Placed")
+    return true;
 
-    if ((ship_last_name.length <= 2) || (ship_last_name.length > 20)) {
-        alert("To Less Characters in Last Name for Shipping Adress");
-        return false;
-    }
-
-
-//street adress validation
-if (ship_street_add == "" || ship_street_add == null) {
-    alert("Street Address nedded for Shipping Adress");
-    return false;
 }
-
-//City validation
-if (ship_city == "" || ship_city == null) {
-    alert("City Name nedded for Shipping Adress");
-    return false;
-}
-
-//Postal-code validation
-if (ship_postal == "" || ship_postal == null) {
-    alert("Postal Code nedded for Shipping Adress");
-    return false;
-}
-
-if (!/^[0-9]+$/.test(ship_postal)) {
-    alert("Only Numeric values allowed");
-    return false;
-}
-
-
-//state validation
-if (ship_state == null || ship_state == "") {
-    alert("State Name nedded for Shipping Adress");
-    return false;
-}
-
-
-var ship_bill_value = document.getElementById("myCheck");
-
-if (ship_bill_value.checked == true) {
-    bill_first_name = ship_first_name;
-    bill_last_name = ship_last_name;
-    bill_street_add = ship_street_add;
-    bill_city = ship_city;
-    bill_postal = ship_postal;
-    bill_state = ship_state;
-
-    flag = true;
-}
-
-else {
-
-//bill first name validation
-var regName = /^[A-Za-z]+$/;
-if(bill_first_name == "" || bill_first_name==null){
-    alert('First Name cannot be empty');
-    return false;
-}
-
-if(!(regName.test(bill_first_name)))
-{
-    alert('Invalid First Name given');
-    return false;
-}
-
-
-if ((bill_first_name.length <= 2) || (bill_first_name.length > 20)) {
-    alert("To Less Characters in First Name for Billing Adress");
-    return false;
-}
-
-//last name validation
-var regName = /^[A-Za-z]+$/;
-    if(bill_last_name == "" || bill_last_name==null){
-        alert('Last Name cannot be empty');
-        return false;
-    }
-    if(!(regName.test(bill_last_name)))
-    {
-        alert('Invalid Last Name given');
-        return false;
-    }
-
-    if ((bill_last_name.length <= 2) || (bill_last_name.length > 20)) {
-        alert("To Less Characters in Last Name for Billing Adress");
-        return false;
-    }
-
-
-//street adress validation
-if (bill_street_add == "" || bill_street_add == null) {
-    alert("Street Address nedded for Billing Adress");
-    return false;
-}
-
-//City validation
-if (bill_city == "" || bill_city == null) {
-    alert("City Name nedded for Billing Adress");
-    return false;
-}
-
-//Postal-code validation
-if (bill_postal == "" || bill_postal == null) {
-    alert("Postal Code nedded for Billing Adress");
-    return false;
-}
-if (!/^[0-9]+$/.test(bill_postal)) {
-    alert("Only Numeric values allowed");
-    return false;
-}
-
-
-//state validation
-if (bill_state == null || bill_state == "") {
-    alert("State Name nedded for Billing Adress");
-    return false;
-}
-flag = true;
-}
-if (flag == true) {
-    Place_Order()
-}
-};
-
-
-// billing and shipping 
-
-
-        function myFunction() {
-            var checkBox = document.getElementById("myCheck");
-            var textShip = document.getElementById("ShippingAd");
-            var textBil = document.getElementById("BillingAd");
-
-            var textShip1 = document.getElementById("ShippingAd1");
-            var textBil1 = document.getElementById("BillingAd1");
-
-            var textShip2 = document.getElementById("ShippingAd2");
-            var textBil2 = document.getElementById("BillingAd2");
-
-            var textShip6 = document.getElementById("ShippingAd6");
-            var textBil6 = document.getElementById("BillingAd6");
-
-
-            var textShip12 = document.getElementById("ShippingAd12");
-            var textBil12 = document.getElementById("BillingAd12");
-
-
-            var textShip9 = document.getElementById("ShippingAd9");
-            var textBil9 = document.getElementById("BillingAd9");
-
-
-
-            if (checkBox.checked == true) {
-                textBil.value = textShip.value;
-            } else {
-                textBil.value = "";
-            }
-
-            if (checkBox.checked == true) {
-                textBil1.value = textShip1.value;
-            } else {
-                textBil1.value = "";
-            }
-
-            if (checkBox.checked == true) {
-                textBil2.value = textShip2.value;
-            } else {
-                textBil2.value = "";
-            }
-
-            if (checkBox.checked == true) {
-                textBil6.value = textShip6.value;
-            } else {
-                textBil6.value = "";
-            }
-
-
-            if ( checkBox.checked == true){
-                textBil12.value = textShip12.value;
-            } else{
-                textBil12.value = "";
-            }
-
-            if ( checkBox.checked == true){
-                textBil9.value = textShip9.value;
-            } else{
-                textBil9.value = "";
-            }
-
-        };
-
-function Place_Order() {
-    // ItemValue = localStorage.getItem("ItemValue");
-    // ItemQuantity = Number(localStorage.getItem("ItemQuantity"));
-
-    // priceToPay = ItemValue * ItemQuantity;
-    // printedText = `total : Quantity: ${ItemQuantity} ItemValue: ${priceToPay}`
-    // alert(printedText);
-
-alert("Your order has been generated");
-
-};
-
 
 
 
--- myscript.js
+++ myscript.js
@@ -9,68 +9,13 @@ function normalImg(x) {
   x.style.width = "70px";
 }
 
+
 //for product to billing page
 
 function myFunction() {
   location.href = "billing.html";
 }
 
-// increment
-//  function increment() {
-//    var quantityitem = document.getElementById("quantity").value;
-//    quantityitem++;
-
-//    if (quantityitem > 1) {
-//       document.querySelector(".minus-btn").setAttribute("disabled","disabled");
-//       document.querySelector(".minus-btn").classList.remove("disabled","disabled");
-//    }
-
-//    document.getElementById("quantity").value = quantityitem;
-//  }
-
-// decrement
-// document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
-// function decrement() {
-
-//     quantityitem = document.getElementById("quantity").value;
-//     quantityitem--;
-
-//     if (quantityitem == 1) {
-//         document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
-//     }
-
-//     document.getElementById("quantity").value = quantityitem;
-// }
-
-// function quantityOfItemInc() {
-//   var quantityitem = document.getElementById("quantity").value;
-//   quantityitem++;
-
-//   if (quantityitem > 1) {
-//     document.querySelector(".minus-btn").removeAttribute("disabled");
-//     document.querySelector(".minus-btn").classList.remove("disabled");
-//   }
-
-
-//   document.getElementById("quantity").value = quantityitem;
-// }
-
-//decreament
-// document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
-// function quantityOfItemDec() {
-
-//   quantityitem = document.getElementById("quantity").value;
-//   quantityitem--;
-
-//   if (quantityitem == 0) {
-//     document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
-//   }
-
-//   document.getElementById("quantity").value = quantityitem;
-// }
-
-
-// increament
 function quantityOfItemInc() {
   var quantityitem = document.getElementById("quantity").value;
   quantityitem++;
@@ -83,7 +28,7 @@ function quantityOfItemInc() {
   document.getElementById("quantity").value = quantityitem;
 }
 
-// decreament
+
 document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
 function quantityOfItemDec() {
 
@@ -96,18 +41,3 @@ function quantityOfItemDec() {
 
   document.getElementById("quantity").value = quantityitem;
 }
-
-// function ItemValidation() {
-//   reg = /^[0-9]+$/;
-//   for (var i = 0; i < price.length; i++) {
-//     if (reg.test(price[i]) || price[i] == ".") {
-//       finalValueOfItem += price[i];
-//     }
-//   }
-//}
-
-// const priceOftheThing = document.getElementById("priceOfThing");
-
-// console.log(priceOftheThing);
-// let price = priceOftheThing[].innerHTML;
-// console.log("Price of Element : ", price);
